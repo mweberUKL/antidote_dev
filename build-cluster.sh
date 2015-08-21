e1dce0ed19a1e8ee265c5ce4e5f1ba3597c0026d
Mathias Weber <mathias@weber-inter.net>
2015-08-21 17:00:29 +0200

    updated script to build cluster

--- build-cluster.sh
+++ build-cluster.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 for d in $DEV/dev*; do $d/bin/antidote start; done
 for d in $DEV/dev{2,3,4,5,6}; do $d/bin/antidote-admin cluster join 'dev1@127.0.0.1'; done
+$DEV/dev1/bin/antidote-admin cluster plan
+$DEV/dev1/bin/antidote-admin cluster commit
